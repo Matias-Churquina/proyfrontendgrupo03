1d0c871cb2071115e73dcda121196b941795451f
Matias-Churquina <166052187+Matias-Churquina@users.noreply.github.com>
2026-07-02 00:52:14 -0300

    Revert "Feature/implementacion vista chofer"

--- src/models/reserva.model.ts
+++
@@ -1,9 +0,0 @@
-export interface Reserva {
-  idReserva: number;
-  cantidadAsientos: number;
-  fechaReserva: Date;
-  importeTotal: number;
-  estadoReserva: 'PENDIENTE' | 'CONFIRMADA' | 'CANCELADA' | 'UTILIZADA' | 'NO_PRESENTADO'; 
-  estadoPago: 'PENDIENTE' | 'PAGADO' | 'REEMBOLSADO';
-  fechaActualizacion: Date;
-}
--- src/models/usuario.model.ts
+++
@@ -1,8 +0,0 @@
-export interface Usuario {
-  idUsuario: number;
-  nombre: string;
-  apellido: string;
-  email: string;
-  telefono: string;
-  activo: boolean;
-}
