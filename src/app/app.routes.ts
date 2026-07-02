802b93aaf2714bda6fad1a3c05a02b5eb3dc59b4
Rodrigo Guanactolay <guanctolayrodrigo6@gmail.com>
2026-07-02 19:45:41 -0300

    Agregado de footer y header.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { Home } from './pages/home/home';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: '',redirectTo: 'Home',pathMatch: 'full' },
+    {path: '**',redirectTo: 'Home'},
+    {path: 'Home', component: Home},
+];
